44e894b6e096634eecc88785868fefa4b7bb59a2
Lera <lerayokovenko098@gmail.com>
2024-11-15 00:53:03 +0200

    edited the projectedited the project

--- merch/src/components/Discount/TShirtCard/index.jsx
+++ merch/src/components/Discount/TShirtCard/index.jsx
@@ -1,10 +1,9 @@
 import './style.css';
 
 const TShirtCard = ({ name, price }) => (
-    <div style={{ textAlign: 'center', width: 150 }}>
-      <img src="https://via.placeholder.com/150" alt={name} style={{ width: '100%' }} />
+    <div style={{ textAlign: 'center', width: 150}}>
       <p>{name}</p>
-      <p style={{ fontWeight: 'bold' }}>{price}</p>
+      <p style={{ fontWeight: 'bold'}}>{price}</p>
     </div>
   );
   
