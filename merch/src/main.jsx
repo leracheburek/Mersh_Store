70f07e139b64bb76f890414b9cf8352c7c5babec
Anastasiia Stelmakh <anastasis.stelmakh@gmail.com>
2024-11-14 20:36:12 +0200

    Add component MainBlock

--- merch/src/main.jsx
+++ merch/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './output.css'
 import Discount from './components/Discount'
+import MainBlock from './components/MainBlock'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <MainBlock/>
     <Discount />
   </StrictMode>,
 )
