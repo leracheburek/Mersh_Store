c075eb25bcf900f22f8560b3620f19388dea1d3f
Anastasiia Stelmakh <anastasis.stelmakh@gmail.com>
2024-11-15 08:46:34 +0200

    change main and advantages block

--- merch/src/components/AdvantagesBlock/index.jsx
+++ merch/src/components/AdvantagesBlock/index.jsx
@@ -10,22 +10,19 @@ function OurAdvantages() {
                     <h3 className="headerblock-advblock">ДОСТАВКА ПО СВІТУ</h3>
                     <p className="textmain-advblock">Здійснюємо доставку ваших замовлень в будь-яку точку планети</p>
                     <p className="text-advblock">Умови доставки читайте нижче</p>
+                    <img src="./src/assets/img/globe.png" alt="globe" className="icon" />
                 </div>
         
-                <div className="card">
+                <div className="cardsecond">
                     <h3 className="headerblock-advblock">ЗРОБЛЕНО В УКРАЇНІ</h3>
                     <p className="textmain-advblock">Всі товари вироблені в Україні з якісних та натуральних тканин</p>
+                    <img src="./src/assets/img/award.png" alt="award" className="iconsecond" />
                 </div>
         
                 <div className="cardthird">
                     <h3 className="headerblock-advblock">ЛЕГКИЙ СПОСІБ ОПЛАТИ</h3>
                     <p className="textmain-advblock">На сайті доступні різні види оплати. Обирайте зручний спосіб саме для вас</p>
                     <p className="text-advblock">Детальніше про способи оплати читайте нижче</p>
-                </div>
-
-                <div class="img-icon">
-                    <img src="./src/assets/img/globe.png" alt="globe" className="icon" />
-                    <img src="./src/assets/img/award.png" alt="award" className="iconsecond" />
                     <img src="./src/assets/img/credit-card-check.png" alt="credit-card-check" className="iconthird" />
                 </div>
             </div>
